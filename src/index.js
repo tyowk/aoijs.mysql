7a2faaa562e2826d3712eb080e942815aeb8b428
Tyowk <tyowk.personal@gmail.com>
2024-12-24 13:31:57 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Events = exports.Database = void 0;
-var Database_1 = require("./classes/Database");
-Object.defineProperty(exports, "Database", { enumerable: true, get: function () { return Database_1.Database; } });
-var Events;
-(function (Events) {
-    Events["Connect"] = "connect";
-    Events["Ready"] = "connect";
-    Events["Disconnect"] = "disconnect";
-    Events["Close"] = "disconnect";
-    Events["Error"] = "error";
-    Events["Acquire"] = "acquire";
-    Events["Release"] = "release";
-    Events["Connection"] = "connection";
-    Events["Enqueue"] = "enqueue";
-})(Events || (exports.Events = Events = {}));
+exports.Database = require('./classes/Database.js').Database;
+exports.Events = {
+    Connect: 'connect',
+    Ready: 'connect',
+    Disconnect: 'disconnect',
+    Close: 'disconnect',
+    Error: 'error',
+    Acquire: 'acquire',
+    Release: 'release',
+    Connection: 'connection',
+    Enqueue: 'enqueue'
+};
