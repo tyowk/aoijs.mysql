fcb1a58da6f4d78f6cd1dc75dba60324c58944b1
Tyowk <tyowk.personal@gmail.com>
2025-07-11 16:18:51 +0700

    Create mysqlSetTimeout.js

---
+++ src/functions/mysqlSetTimeout.js
@@ -0,0 +1,35 @@
+const { Time } = require("aoi.js/src/core/Time.js");
+const { Timeout } = require("../classes/Timeout.js");
+
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+    const [name, duration, timeoutData, returnID = "false"] = data.inside.splits;
+
+    if (!name) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Timeout Name Provided");
+    const time = isNaN(duration) ? Time.parse(duration)?.ms : Number(duration);
+    let tdata = {};
+
+    if (timeoutData.addBrackets().startsWith("{")) {
+        try {
+            tdata = JSON.parse(timeoutData.addBrackets());
+        } catch (e) {
+            d.aoiError.fnError(d, "custom", { inside: data.inside });
+        }
+    } else {
+        for (let timeData of timeoutData.split("\n")) {
+            timeData = timeData.split(":");
+            tdata[timeData[0].trim()] = timeData[1].trim();
+        }
+    }
+
+    const timeout = Timeout(d, name.addBrackets(), time, tdata);
+
+    if (returnID === "true") {
+        data.result = timeout;
+    }
+
+    return {
+        code: d.util.setCode(data)
+    };
+};
